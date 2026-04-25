0f822870b3147c31c767b361abe1a89522a68355
Krishna-devk <goldyagarwal81@gmail.com>
2026-04-25 17:30:30 +0530

    done

--- server.js
+++ server.js
@@ -4,16 +4,7 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/db")
 
 connectToDB()
-
-// For Local Development
-if (typeof addEventListener === 'undefined') {
-    const PORT = process.env.PORT || 3000
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}
-
-// For Cloudflare Workers
-module.exports = {
-    fetch: (request, env, ctx) => app(request, env, ctx)
-}
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
